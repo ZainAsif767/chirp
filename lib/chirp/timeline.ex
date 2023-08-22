2c7502060af57628ca0373c529069dfc6f5849b2
Zainasif767 <zainclasher2002@gmail.com>
2023-08-23 00:00:38 +0500

    inc_likes and inc_reposts module

--- lib/chirp/timeline.ex
+++ lib/chirp/timeline.ex
@@ -21,6 +21,22 @@ defmodule Chirp.Timeline do
     Repo.all(from p in Post, order_by: [desc: p.id])
   end
 
+  def inc_likes(%Post{id: id}) do
+    {1, [post]} =
+      from(p in Post, where: p.id == ^id, select: p)
+      |> Repo.update_all(inc: [like_count: 1])
+
+    broadcast({:ok, post}, :post_updated)
+  end
+
+  def inc_reposts(%Post{id: id}) do
+    {1, [post]} =
+      from(p in Post, where: p.id == ^id, select: p)
+      |> Repo.update_all(inc: [reposts_count: 1])
+
+    broadcast({:ok, post}, :post_updated)
+  end
+
   @doc """
   Gets a single post.
 
